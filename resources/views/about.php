79d64963e885e28e4474a05f74f7991c4e3718a2
Ahmad-Abbas-1 <abbs.os.ahmad@gmail.com>
2025-09-09 13:31:00 +0200

    Video-2 afgekeken en een contact en about page gemaakt

---
+++ resources/views/about.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<head>
+    <title>about page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+    <h1> about page </h1>
+</body>
+
+
+</html>
---
+++ resources/views/contact.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<head>
+    <title>contact page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+<body>
+
+    <h1> contact page </h1>
+</body>
+
+
+</html>
