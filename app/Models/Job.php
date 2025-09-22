b6009d1ca6957bc9f50058a16ddc6796f7f037e1
Ahmad-Abbas-1 <abbs.os.ahmad@gmail.com>
2025-09-22 10:03:51 +0200

    video-9 afgekeken, Job model aangemaakt om gegevens uit de database te halen

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,39 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+
+class Job extends Model 
 {
-    public static function all(): array
-    {
-        return $jobs = [
-            [
-                'id' => 1,
-                'title' => 'Captain',
-                'salary' => '1 willion doubloons',
-            ],
-            [
-                'id' => 2,
-                'title' => 'bilge rat',
-                'salary' => '1 dolar',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Landlubber',
-                'salary' => '0 riches, 0 glory, no treasure😡😡😡😡😡',
-            ]
-        ];
-    }
+    protected $table = 'job_listings';
 
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (! $job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
